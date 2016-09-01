a3dea607599809aedf4bfd7d22aece18a8e72366
brunotriptribus <bruno@triptribus.com>
2016-08-31 21:17:28 -0400

    ambitionprod

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ambitieux/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ local/.bash_logout
@@ -0,0 +1,1 @@
+/home/ambitieux/.bash_logout
